cab468b0458dffd58b2a221e5c2d4e01c924b9db
Emeka David <emekad745@gmail.com>
2024-08-03 20:19:30 +0100

    Added Privacy policiy

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = ()=>{
         <footer>
             <img width='88px' src={careLogo} alt="care tech logo" />
             <p>&copy;copy right 2024 CARE TECH HUB. All rights reserved.
-                <a href="../Legal/PRIVACY_POLICY[1].docx"> Privacy Policy</a> | <a href="">Terms of Service</a></p>
+                <a href="../Legal/~$IVACY_POLICY[1].docx"> Privacy Policy</a> | <a href="">Terms of Service</a></p>
         </footer>
     )
 }
